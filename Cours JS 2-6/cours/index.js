4b3581256fd5f878ea5c1c5525ef8dec697e889d
Balevicky <balevictorien@yahoo.fr>
2025-01-08 19:09:03 +0000

    modified style.css , index.html and index.js

--- Cours JS 2-6/cours/index.js
+++ Cours JS 2-6/cours/index.js
@@ -47,4 +47,58 @@ btn2.addEventListener("click", () => {
   response.classList.add("show-response");
   response.style.background = "red";
 });
+// ========= les priorité en css
+// <div style dans la base> >#id> .class> baliseHTML
 //------------------------------------------
+
+// ==========================MOUSE EVENTS=============
+const mousemove = document.querySelector(".mousemove");
+// console.log(mousemove);
+window.addEventListener("mousemove", (e) => {
+  // console.log("mousemove!!");
+  // console.log(e);
+  // console.log(e.target);
+  // ------- recuperer la position de la souris
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+// -------------------
+window.addEventListener("mousedown", () => {
+  // console.log("test");
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+  mousemove.style.border = "2px solid pink";
+});
+// ------------
+window.addEventListener("mouseup", () => {
+  // console.log("test");
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+// ------------
+questioncontainer.addEventListener("mouseenter", () => {
+  questioncontainer.style.background = "red";
+});
+// ------------
+questioncontainer.addEventListener("mouseout", () => {
+  questioncontainer.style.background = "green";
+});
+// --------------
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(5deg)";
+});
+// ===========KEYPRESS EVENT=================
+const keypressContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+document /
+  addEventListener("keypress", (e) => {
+    // console.log(e.key);
+    key.textContent += e.key;
+    if (key.textContent === "goli") {
+      keypressContainer.style.background = "green";
+    } else if (key.textContent === "bale") {
+      keypressContainer.style.background = "blue";
+    } else {
+      keypressContainer.style.background = "pink";
+    }
+  });
+// ------------------ ajout de son en js
